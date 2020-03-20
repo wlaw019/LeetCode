d8ffd5a871d38b24d1286498525a8e0c35cd38c7
Wincy LAW <wincylaw@Wincys-MacBook-Pro.local>
2020-03-19 23:31:43 -0700

    updated function

--- 03112020App.js
+++ 03112020App.js
@@ -1,12 +1,40 @@
 //////////////////////
 // 58) Length of Last Word
 //////////////////////
-lengthOfLastWord = (s) => {
-  sTrim = s.trim();
-  console.log(sTrim.length-1-sTrim.lastIndexOf(" "));
-  return sTrim.length-1-sTrim.lastIndexOf(" ")
+// lengthOfLastWord = (s) => {
+//   sTrim = s.trim();
+//   console.log(sTrim.length-1-sTrim.lastIndexOf(" "));
+//   return sTrim.length-1-sTrim.lastIndexOf(" ")
+//
+//
+// }
+//
+// lengthOfLastWord("Hello World  ")
 
 
+// console.log(employees[0].reports[0].reports[0]);
+
+RemoveLastReport = (s) => {
+  let employees = [
+    {
+      name: "Aiden",
+      reports:[{name:"Nico", reports:[{name: "Katherine"}]}]
+    }
+  ]
+
+  // let modifyS = JSON.stringify(s);
+  modifyS = s + ".reports[0]"
+  console.log(modifyS);
+
+  // let ans = eval(modifyS)
+
+  console.log(eval(modifyS));
+
+  // console.log(s.lastIndexOf("reports"));
+  // let sliceIndex = s.lastIndexOf("reports");
+  //
+  // console.log(s.slice(0, sliceIndex));
 }
 
-lengthOfLastWord("Hello World  ")
+// RemoveLastReport('employees[0]reports[1]reports[5]reports[3]')
+RemoveLastReport("employees[0].reports[0]")
